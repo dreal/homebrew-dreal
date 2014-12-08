420ed55c79dc0a36672d9f84cd29a198578ff657
Soonho Kong <soonhok@cs.cmu.edu>
2014-12-08 11:20:59 -0500

    fix(dreal.rb): point to the blessed repo dreal/dreal

---
+++ dreal.rb
@@ -0,0 +1,55 @@
+require "formula"
+
+class Dreal < Formula
+  homepage "http://dreal.github.io"
+  url "https://github.com/dreal/dreal.git"
+  version "2.14.12-gitaa7f0182ab2e9fa5cbd42bd73509a5bd330aea8d"
+
+  bottle do
+    root_url 'https://dreal.github.io/homebrew-dreal'
+    sha1 'a3c675595fcc979a046d05aca95473422bdd05dd' => :yosemite
+    sha1 '7439f634d7884ebe16ea79fbd2a6c93823e30566' => :mavericks
+  end
+
+  # Required
+  depends_on 'automake'         => :build
+  depends_on 'autoconf'         => :build
+  depends_on 'libtool'          => :build
+  depends_on 'pkg-config'       => :build
+  depends_on 'objective-caml'   => :build
+  depends_on 'opam'             => :build
+  depends_on 'cmake'            => :build
+  depends_on 'wget'             => :build
+  depends_on 'google-perftools' => :optional
+
+  def install
+    args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}",
+            "-DCMAKE_BUILD_TYPE=Release"]
+    mkdir 'build' do
+      # Compile tools (Ocaml)
+      if ! Dir.exists?(ENV['HOME'] + "/.opam")
+          system "opam", "init", "--yes"
+      end
+      # system "opam", "switch", "4.02.1"
+      # ENV['PATH'] = ENV['HOME'] + "/.opam/4.02.1/bin" + ":" + ENV['PATH']
+      ENV['PATH'] = ENV['HOME'] + "/.opam/system/bin" + ":" + ENV['PATH']
+      puts "PATH= " + ENV['PATH']
+      system "opam", "install", "--yes", "oasis", "batteries", "ocamlfind"
+      system "make", "-C", "../tools", "setup.ml", "setup.data"
+      system "make", "-C", "../tools"
+      # Compile dReal (C++)
+      system "cmake", "../src", *args
+      system "make", "-j"
+      system "make", "-j", "install"
+    end
+  end
+
+  test do
+    system "make", "-j", "test"
+  end
+
+  def caveats; <<-EOS.undent
+      dReal installed.
+    EOS
+  end
+end
