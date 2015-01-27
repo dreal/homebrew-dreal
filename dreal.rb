82b8d335f42009353f7814c7b291e976722808aa
Soonho Kong <soonhok@cs.cmu.edu>
2015-01-27 06:49:36 -0500

    Bottle: 2.15.01-gitc5b3c970a3ea157b84e09099ccbd59d865a53861

--- dreal.rb
+++ dreal.rb
@@ -5,11 +5,11 @@ class Dreal < Formula
   url "https://github.com/soonhokong/dreal.git"
   version "2.15.01-gitc5b3c970a3ea157b84e09099ccbd59d865a53861"
 
-##BOTTLE_COMMENT##  bottle do
-##BOTTLE_COMMENT##    root_url 'https://dreal.github.io/homebrew-dreal'
-##BOTTLE_COMMENT##    sha1 '##BOTTLE_YOSEMITE_HASH##' => :yosemite
-##BOTTLE_COMMENT##    sha1 '##BOTTLE_MAVERICKS_HASH##' => :mavericks
-##BOTTLE_COMMENT##  end
+  bottle do
+    root_url 'https://dreal.github.io/homebrew-dreal'
+    sha1 '00bc68bf0c3031d68f9f4a8f042f86be1504237f' => :yosemite
+    sha1 '##BOTTLE_MAVERICKS_HASH##' => :mavericks
+  end
 
   # Required
   depends_on 'automake'         => :build
