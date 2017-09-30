18c5f6f607ef82747ef55227307378b704c5f9b1
Soonho Kong <soonho.kong@tri.global>
2017-09-29 23:49:29 -0400

    Update to 4.17.09.4

--- dreal.rb
+++ dreal.rb
@@ -1,14 +1,14 @@
 class Dreal < Formula
   homepage "http://dreal.github.io"
-  url "https://github.com/dreal/dreal4/archive/4.17.09.3.tar.gz"
-  sha256 "57beb7eebe87d8c17625e02feb8f22e831dc8110c2d7f610f0e7a68965c7b5bc"
-  version "4.17.09.3"
+  url "https://github.com/dreal/dreal4/archive/4.17.09.4.tar.gz"
+  sha256 "bf5d505541498dbd13b0196ac6d649d674841925dddcfd4f722bce7d9ff798e6"
+  version "4.17.09.4"
 
-  bottle do
-    root_url 'https://dl.bintray.com/dreal/homebrew-dreal'
-    sha256 "4f122c35de62e1031686b4902199e444480884867525fba97a397d2c2b2f7062" => :high_sierra
-    sha256 "76fd8632a8a429b5ceca2221c546ef71828404866cdfea23c5f8f5e5b6595004" => :sierra
-  end
+#  bottle do
+#    root_url 'https://dl.bintray.com/dreal/homebrew-dreal'
+#    sha256 "4f122c35de62e1031686b4902199e444480884867525fba97a397d2c2b2f7062" => :high_sierra
+#    sha256 "76fd8632a8a429b5ceca2221c546ef71828404866cdfea23c5f8f5e5b6595004" => :sierra
+#  end
 
   # Required
   depends_on 'bazel'                => :build
