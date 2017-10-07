a62afaaa1195e3abf4a80709925971cccd29bbfe
Soonho Kong <soonho.kong@tri.global>
2017-10-07 11:27:57 -0400

    Update to 4.17.10

--- dreal.rb
+++ dreal.rb
@@ -1,12 +1,12 @@
 class Dreal < Formula
   homepage "http://dreal.github.io"
-  url "https://github.com/dreal/dreal4/archive/4.17.09.4.tar.gz"
-  sha256 "bf5d505541498dbd13b0196ac6d649d674841925dddcfd4f722bce7d9ff798e6"
-  version "4.17.09.4"
+  url "https://github.com/dreal/dreal4/archive/4.17.10.tar.gz"
+  sha256 "a8bb8d8b517ffe77a8651e804c67c3ce933906101835eb4a6697589bd723daf7"
+  version "4.17.10"
 
   bottle do
     root_url 'https://dl.bintray.com/dreal/homebrew-dreal'
-    sha256 "48587d4debd7e657caea991e39c208c1b94f6d2b0c3a6c25125e31211b4c62ad" => :high_sierra
+#    sha256 "48587d4debd7e657caea991e39c208c1b94f6d2b0c3a6c25125e31211b4c62ad" => :high_sierra
 #    sha256 "76fd8632a8a429b5ceca2221c546ef71828404866cdfea23c5f8f5e5b6595004" => :sierra
   end
 
@@ -14,6 +14,7 @@ class Dreal < Formula
   depends_on 'bazel'                => :build
   depends_on 'pkg-config'           => :build
   depends_on 'dreal-deps/ibex/ibex'
+  depends_on 'nlopt'
 
   needs :cxx11
 
