1ba102e575567325274497fd7c64948af8b2e4bc
Soonho Kong <soonho.kong@tri.global>
2017-10-23 14:59:20 -0400

    Update to 4.17.10.4

--- dreal.rb
+++ dreal.rb
@@ -1,15 +1,13 @@
 class Dreal < Formula
   homepage "http://dreal.github.io"
-  url "https://github.com/dreal/dreal4/archive/4.17.10.3.tar.gz"
-  sha256 "f6a3531b304950bbef4445a973e35a575ec241fc14a162c60536f54c40d9ae78"
-  version "4.17.10.3"
+  url "https://github.com/dreal/dreal4/archive/4.17.10.4.tar.gz"
+  sha256 "fa473acc2dbf54ea513fc1a02265aa9e5aec206e407cd5db85f9b01d8ca7ba58"
 
   bottle do
     root_url 'https://dl.bintray.com/dreal/homebrew-dreal'
     cellar :any
-    rebuild 1
-    sha256 "13b6f49fa6695bed87d90fded5a3d28620c78924e1ae59f8a93adffdcbf723ca" => :high_sierra
-    sha256 "9ad2ca43bd669aa5dbc2da54eeab8b1a56788260d9e99dc111eb507ec20c286c" => :sierra
+    sha256 "5a2b91964770211ce7e71dac67d5dbc3c8e70519d5c7fb744488a72e8afcdbca" => :high_sierra
+#    sha256 "" => :sierra
   end
 
   # Required
