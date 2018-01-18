7e416410609a24becf457cf46f4778d7e3843d8c
Soonho Kong <soonho.kong@tri.global>
2018-01-17 20:47:32 -0500

    Add 10.11 bottle

--- dreal.rb
+++ dreal.rb
@@ -6,8 +6,9 @@ class Dreal < Formula
   bottle do
     root_url 'https://dl.bintray.com/dreal/homebrew-dreal'
     cellar :any
-    sha256 "bc93726a76b0a124dc5c9afcbc4627c8632201daae13179f9d28ddac1b18e25e" => :high_sierra
-    sha256 "dc03448ac8660e112a7960daff9ccfbc85b13ace0fadb1969e3f1592c119fdaf" => :sierra
+     sha256 "0ce07f24d00dae5eaf6aca3a00e727e6e7a8040e7afc826c8e14080705beb6e3" => :el_capitan
+     sha256 "bc93726a76b0a124dc5c9afcbc4627c8632201daae13179f9d28ddac1b18e25e" => :high_sierra
+     sha256 "dc03448ac8660e112a7960daff9ccfbc85b13ace0fadb1969e3f1592c119fdaf" => :sierra
   end
 
   # Required
