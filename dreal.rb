c1b66a5494b730f8655239985bcf0b491bdf5e9e
Soonho Kong <soonho.kong@tri.global>
2018-02-23 01:35:17 -0500

    Update 4.18.02.4

--- dreal.rb
+++ dreal.rb
@@ -1,14 +1,15 @@
 class Dreal < Formula
   homepage "http://dreal.github.io"
-  url "https://github.com/dreal/dreal4/archive/4.18.02.2.tar.gz"
-  sha256 "51f576a8881d3f916293b2ff56dc7038638e694a6ed6da9c1503cf0e37957d0d"
-
+  url "https://github.com/dreal/dreal4/archive/4.18.02.4.tar.gz"
+  sha256 "7f2f534a1f1314bb9e2d58b7af66989ce0538208528fd63ed2fd5efc4457c162"
+  version "4.18.02.4"
+  
   bottle do
     root_url 'https://dl.bintray.com/dreal/homebrew-dreal'
     cellar :any
-      sha256 "3d7971bdd0f29e2babaa2a09e936878dd44ed277d60b9d42de70f4efa29b771c" => :el_capitan
-      sha256 "58fba701488bd73814448b1de0948407a16b0c4ea48d45b5c70fd6faae55e5ce" => :sierra
-      sha256 "9c8afa5d2f640d3be96c5658aed7632a9f5a2b4ca7ff7d1fc459ac5931323a63" => :high_sierra
+      # sha256 "3d7971bdd0f29e2babaa2a09e936878dd44ed277d60b9d42de70f4efa29b771c" => :el_capitan
+      # sha256 "58fba701488bd73814448b1de0948407a16b0c4ea48d45b5c70fd6faae55e5ce" => :sierra
+      sha256 "83b6a76ad0b288bdd17eab535e476ff1ddcc54106a159905634c84ea7c66e07a" => :high_sierra
   end
 
   # Required
@@ -25,7 +26,7 @@ class Dreal < Formula
     bin.install "opt/dreal/#{version}/bin/dreal"
     lib.install "opt/dreal/#{version}/lib/libdreal.so"
     include.install "opt/dreal/#{version}/include/dreal"
-    inreplace "opt/dreal/#{version}/lib/pkgconfig/dreal.pc", "HOMEBREW_CELLAR", "#{HOMEBREW_PREFIX}/Cellar"
+    inreplace "opt/dreal/#{version}/lib/pkgconfig/dreal.pc", "HOMEBREW_PREFIX", "#{HOMEBREW_PREFIX}"
     (lib+"pkgconfig").install "opt/dreal/#{version}/lib/pkgconfig/dreal.pc"
   end
 end
