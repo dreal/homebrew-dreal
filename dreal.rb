99a5c3616f04d2ad2a62e785c607bfe1cf994f19
Soonho Kong <soonho.kong@tri.global>
2018-03-25 23:28:06 -0400

    Add bottle for 10.13

--- dreal.rb
+++ dreal.rb
@@ -8,7 +8,7 @@ class Dreal < Formula
     cellar :any
       # sha256 "17f18baccadc8055389d83d4e2a682115e8e14291cc575a4f33acf76bed2b696" => :el_capitan
       # sha256 "b9ede02afd86feefc188559ec489133e4bb2898fab1a43b7fe65ed4d4b8bf529" => :sierra
-      # sha256 "83b6a76ad0b288bdd17eab535e476ff1ddcc54106a159905634c84ea7c66e07a" => :high_sierra
+      sha256 "7517e4a3ac1ec66ce5c571ef0861665f218af9bbbd6db0474ea2e0ec3c84bf9b" => :high_sierra
   end
 
   # Required
