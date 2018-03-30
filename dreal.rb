cbd2606ed44114d22282415fa61d0c9b74c37af3
Soonho Kong <soonho.kong@tri.global>
2018-03-30 19:59:55 -0400

    Update formula
     - Use tar -C and --strip-components to install
     - Remove caveat

--- dreal.rb
+++ dreal.rb
@@ -7,9 +7,9 @@ class Dreal < Formula
   bottle do
     root_url "https://dl.bintray.com/dreal/homebrew-dreal"
     cellar :any
-    sha256 "a2a8aef14e0f16768331fde0e4969fd80bd8b9a6aa1f1e6f1c80bb606aaf27c4" => :el_capitan
-    sha256 "7d91cbfbff01c448ea9eaecd041ddc31f596d88796a58294fff2107db5966baa" => :sierra
-    sha256 "982039daa4e63c9c6b76ac54c5acfb6944e7054574cafb370b9636f2ed2b9590" => :high_sierra
+    # sha256 "a2a8aef14e0f16768331fde0e4969fd80bd8b9a6aa1f1e6f1c80bb606aaf27c4" => :el_capitan
+    # sha256 "7d91cbfbff01c448ea9eaecd041ddc31f596d88796a58294fff2107db5966baa" => :sierra
+    # sha256 "982039daa4e63c9c6b76ac54c5acfb6944e7054574cafb370b9636f2ed2b9590" => :high_sierra
   end
 
   # Required
@@ -23,19 +23,11 @@ class Dreal < Formula
 
   def install
     system "bazel", "build", "--compilation_mode=opt", "//:archive"
-    system "tar", "xvf", "bazel-bin/archive.tar.gz"
-    bin.install "opt/dreal/#{version}/bin/dreal"
-    lib.install "opt/dreal/#{version}/lib/libdreal.so"
-    include.install "opt/dreal/#{version}/include/dreal"
-    inreplace "opt/dreal/#{version}/lib/pkgconfig/dreal.pc", "HOMEBREW_PREFIX", HOMEBREW_PREFIX.to_s
-    (lib/"pkgconfig").install "opt/dreal/#{version}/lib/pkgconfig/dreal.pc"
-    (lib/"python2.7").install "opt/dreal/#{version}/lib/python2.7/site-packages"
-  end
-
-  def caveats; <<~EOS
-    To use Python binding, add the following path to PYTHONPATH environment variable:
-      #{HOMEBREW_PREFIX}/opt/dreal/lib/python2.7/site-packages
-    EOS
+    # files in archive.tar.gz have `./opt/dreal/<version>` prefix
+    # (4-level). It uncompresses files at #{prefix} whil stripping the
+    # prefix.
+    system "tar", "xvf", "bazel-bin/archive.tar.gz", "--strip-components", "4", "-C", prefix.to_s
+    inreplace "#{lib}/pkgconfig/dreal.pc", "HOMEBREW_PREFIX", HOMEBREW_PREFIX.to_s
   end
 
   test do
