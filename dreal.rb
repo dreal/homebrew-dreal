d3f5748ff1bdb76ef70eb849e3d1df42a63882be
Soonho Kong <soonho.kong@tri.global>
2018-03-30 17:16:05 -0400

    Fixup warnings from brew audit

--- dreal.rb
+++ dreal.rb
@@ -1,23 +1,23 @@
 class Dreal < Formula
-  homepage "http://dreal.github.io"
-  version "4.18.03.2"
-  url "https://github.com/dreal/dreal4/archive/#{version}.tar.gz"
+  desc "SMT Solver for Nonlinear Theories of Reals"
+  homepage "https://dreal.github.io"
+  url "https://github.com/dreal/dreal4/archive/4.18.03.2.tar.gz"
   sha256 "532a5392db7e106a5160948b4b97fb3bcdd0779c19369dc557693513830d5427"
-  
+
   bottle do
-    root_url 'https://dl.bintray.com/dreal/homebrew-dreal'
+    root_url "https://dl.bintray.com/dreal/homebrew-dreal"
     cellar :any
-      sha256 "a2a8aef14e0f16768331fde0e4969fd80bd8b9a6aa1f1e6f1c80bb606aaf27c4" => :el_capitan
-      sha256 "7d91cbfbff01c448ea9eaecd041ddc31f596d88796a58294fff2107db5966baa" => :sierra
-      sha256 "982039daa4e63c9c6b76ac54c5acfb6944e7054574cafb370b9636f2ed2b9590" => :high_sierra
+    sha256 "a2a8aef14e0f16768331fde0e4969fd80bd8b9a6aa1f1e6f1c80bb606aaf27c4" => :el_capitan
+    sha256 "7d91cbfbff01c448ea9eaecd041ddc31f596d88796a58294fff2107db5966baa" => :sierra
+    sha256 "982039daa4e63c9c6b76ac54c5acfb6944e7054574cafb370b9636f2ed2b9590" => :high_sierra
   end
 
   # Required
-  depends_on 'bazel'                => :build
-  depends_on 'pkg-config'           => :build
-  depends_on 'python@2'             => :build
-  depends_on 'nlopt'
-  depends_on 'dreal-deps/ibex/ibex@2.6.5'
+  depends_on "bazel"                => :build
+  depends_on "pkg-config"           => :build
+  depends_on "python@2"             => :build
+  depends_on "nlopt"
+  depends_on "dreal-deps/ibex/ibex@2.6.5"
 
   needs :cxx14
 
@@ -36,5 +36,9 @@ class Dreal < Formula
     To use Python binding, add the following path to PYTHONPATH environment variable:
       #{HOMEBREW_PREFIX}/opt/dreal/lib/python2.7/site-packages
     EOS
+  end
+
+  test do
+    system "#{bin}/dreal"
   end
 end
