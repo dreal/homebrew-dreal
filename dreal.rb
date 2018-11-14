625eae3c4a0a9c8360a9e48e0838f744ab08e3fb
Soonho Kong <soonho.kong@tri.global>
2018-11-14 18:04:07 -0500

    Update dreal.rb

--- dreal.rb
+++ dreal.rb
@@ -4,13 +4,14 @@ class Dreal < Formula
   url "https://github.com/dreal/dreal4/archive/4.18.11.2.tar.gz"
   sha256 "81625cd4be7c3f88ee997ae8639ae05b039cc12ff976547c393a37aec63f7d30"
   head "https://github.com/dreal/dreal4.git"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/dreal/homebrew-dreal"
     cellar :any
-    sha256 "9a789f1337aa9662e14d8391de9482850805d282baf1f95eb5ea570b4c33cb35" => :sierra
-    sha256 "096b3a4e7cb8ae4e18baee4b416e5b5f4720acf12230f34971b2136adc6b7017" => :high_sierra
-    sha256 "9d43f40c9440bf9c08e53ad0eb148d3720c78874eb61d025a3f787fa89742f1e" => :mojave
+#    sha256 "" => :sierra
+    sha256 "bd5b067c1c799987bd4b8a4420fac923682be95afc0e3195dbcae17416cf45c0" => :high_sierra
+    sha256 "9a88dd731775292bde628cde53f67d005fd71c1879e26d46f8bddd828b5c90a5" => :mojave
   end
 
   # Required
