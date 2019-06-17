6ac2dc74568b29468a5cbc830ddd8bedd0526a94
Soonho Kong <soonho.kong@tri.global>
2019-06-17 14:54:48 -0400

    version: 4.19.06.3

--- dreal.rb
+++ dreal.rb
@@ -1,16 +1,16 @@
 class Dreal < Formula
   desc "SMT Solver for Nonlinear Theories of Reals"
   homepage "https://dreal.github.io"
-  url "https://github.com/dreal/dreal4/archive/4.19.06.2.tar.gz"
-  sha256 "f5f9ec1a7a4b1dd85ffdef665b25281ecf68ff17665b1915cd42d8170d704aa8"
+  url "https://github.com/dreal/dreal4/archive/4.19.06.3.tar.gz"
+  sha256 "84ad7d1eb245f234e06ba43b2a6cd913048bea0a901d88cd99cf6e7cf0ad00ac"
   head "https://github.com/dreal/dreal4.git"
 
   bottle do
     root_url "https://dl.bintray.com/dreal/homebrew-dreal"
     cellar :any
-    sha256 "b2ff0bb92790e80903ae672d692f5d9c045f5c99bf353d2eb1279e5db8eb5845" => :sierra
-    sha256 "79ee92ca9182160b775990cd6d1487bf149abb778a4b509cef8aa9ddada8403a" => :high_sierra
-    sha256 "f878ac1228c7eba715c923e28433434e0f032a59048837347a653302831e4605" => :mojave
+    sha256 "2ef7a504a89aa4d8dd32dc5f60014a1ff01a477095edd6e4b52bf347e3c07cf0" => :sierra
+    sha256 "cf7390a98655df16dd5c64019713a0ad80c0af529825f4cbfc617d70994125e2" => :high_sierra
+    sha256 "d1edc119a005786d935ed549000f8b0199dd5e758fbda73d5cbac345152b6513" => :mojave
   end
 
   # Required
@@ -23,7 +23,7 @@ class Dreal < Formula
   depends_on "nlopt"
 
   def install
-    system "bazel", "build", "--compilation_mode=opt", "//:archive"
+    system "bazel", "build", "--compilation_mode=opt", "--host_force_python=PY2", "//:archive"
     # files in archive.tar.gz have `./opt/dreal/<version>` prefix
     # (4-level). It uncompresses files at #{prefix} whil stripping the
     # prefix.
