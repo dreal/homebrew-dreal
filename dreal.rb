23ba27c39ae334a5e6dc8df6e38004bd8d49227f
Soonho Kong <soonho.kong@tri.global>
2021-04-09 11:35:28 -0400

    Fix style

--- dreal.rb
+++ dreal.rb
@@ -1,3 +1,6 @@
+# typed: false
+# frozen_string_literal: true
+
 class Dreal < Formula
   desc "SMT Solver for Nonlinear Theories of Reals"
   homepage "https://dreal.github.io"
@@ -7,11 +10,10 @@ class Dreal < Formula
 
   bottle do
     root_url "https://dl.bintray.com/dreal/homebrew-dreal"
-    cellar :any
-    sha256 "806c1397c2523c9091fd87c8ac6d5a3d319ca8c1ed999dd3f9bf777032a55ee0" => :high_sierra
-    sha256 "267d41d00bad1694d9c24c826a670ae8a5074ce736d89a1da0e72199bef2b61b" => :mojave
-    sha256 "6242e89c780ce30c33ba8fb562a89e4f46be6e1736ee47455b21df55dbfc95de" => :catalina
-    sha256 "e36b5953a50a3d3d391f2e0b6c7ff38187d1dae02a5dda223a8ef8e1f4efd986" => :big_sur
+    sha256 cellar: :any, high_sierra: "806c1397c2523c9091fd87c8ac6d5a3d319ca8c1ed999dd3f9bf777032a55ee0"
+    sha256 cellar: :any, mojave:      "267d41d00bad1694d9c24c826a670ae8a5074ce736d89a1da0e72199bef2b61b"
+    sha256 cellar: :any, catalina:    "6242e89c780ce30c33ba8fb562a89e4f46be6e1736ee47455b21df55dbfc95de"
+    sha256 cellar: :any, big_sur:     "e36b5953a50a3d3d391f2e0b6c7ff38187d1dae02a5dda223a8ef8e1f4efd986"
   end
 
   # Required
