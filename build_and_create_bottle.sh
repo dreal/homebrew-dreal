5b3c2bc787af989228fe984e0ea2e35ce018615a
Soonho Kong <soonho.kong@tri.global>
2021-04-09 16:01:45 -0400

    Fix build_and_create_bottle.sh

--- build_and_create_bottle.sh
+++ build_and_create_bottle.sh
@@ -4,3 +4,6 @@ cp ./dreal.rb /usr/local/Homebrew/Library/Taps/dreal/homebrew-dreal/dreal.rb
 brew rm dreal -f --ignore-dependencies
 brew install dreal --build-bottle
 brew bottle dreal --verbose
+
+# Reset homebrew-dreal repo
+git -C /usr/local/Homebrew/Library/Taps/dreal/homebrew-dreal reset --hard origin/master
