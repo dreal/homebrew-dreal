a8a427b06e342957fa7911be833f74e9a4f90f82
Soonho Kong <soonho.kong@tri.global>
2021-06-03 08:32:16 -0400

    version 4.21.06.1

--- Formula/dreal.rb
+++ Formula/dreal.rb
@@ -2,18 +2,18 @@
 # frozen_string_literal: true
 
 class Dreal < Formula
-  VERSION = "4.20.12.1"
+  VERSION = "4.21.06.1"
   desc "SMT Solver for Nonlinear Theories of Reals"
   homepage "https://dreal.github.io"
   url "https://github.com/dreal/dreal4/archive/#{VERSION}.tar.gz"
-  sha256 "8a8a726a370700e386ee337adf4d15a6cdc882285f29a7b6afb8f4b05c5f13b9"
+  sha256 "b9c2f2f0d265af4f1061a8491fa9b6771b2465a19fe9c47d4f19f174c14993dd"
   head "https://github.com/dreal/dreal4.git"
 
   bottle do
     root_url "https://github.com/dreal/dreal4/releases/download/#{VERSION}"
-    sha256 cellar: :any, mojave: "65ca699bb79a9823d9235ff0f65787cc7116c56bfa55801581bf6d90a9f4b4b3"
-    sha256 cellar: :any, catalina: "de828a57a60a298b921143b5389a0ce34632a8302b703b04a720a726cf7d238f"
-    sha256 cellar: :any, big_sur: "45ef8e343efc2c6c98aff1013e3c31b3b5f1e3739faff9bfc2c1859314606f22"
+    sha256 cellar: :any, mojave: "898227b6fd230665f99e3581a090f4f96fce339df83a141f8f0737eacf695f92"
+    sha256 cellar: :any, catalina: "d009487146acac3ac3cfe7fad2c488fb91b41d63c0493172330a0c9242d80b78"
+    sha256 cellar: :any, big_sur: "58c446d416a195ca19adb4190f91c8f2ec5cb8ee147d540266a96aa7fad4268f"
   end
 
   # Required
